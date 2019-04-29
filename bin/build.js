3eedf56b311d88ed13f131ee606add72a6dda87f
Baird-weng <644672334@qq.com>
2019-04-29 18:32:38 +0800

    添加入口

---
+++ bin/build.js
@@ -0,0 +1,13 @@
+const creator = require('../lib/creator')
+const fs = require('fs-extra')
+const program = require('commander')
+const path = require('path')
+program
+  .version(fs.readJsonSync(path.join(__dirname, '../package.json')).version, '-v, --version')
+  .command('create <projectName>')
+  .description('使用 BW 创建工程')
+  .action((projectName) => {
+    require('../lib/creator')(projectName)
+  })
+
+program.parse(process.argv)
